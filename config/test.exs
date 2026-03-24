552fa1727e2885214d771332e97e5e99953bd82e
Adam Zaninovich <adam.zaninovich@gmail.com>
2026-03-23 22:27:23 -0700

    Fix SQLite "Database busy" contention in test suite
    Bump busy_timeout to 10s and cap async parallelism to 4 processes
    to avoid saturating SQLite's single-writer lock.

--- config/test.exs
+++ config/test.exs
@@ -9,7 +9,7 @@ config :noter, Noter.Repo,
   database: Path.expand("../noter_test.db", __DIR__),
   pool_size: 5,
   pool: Ecto.Adapters.SQL.Sandbox,
-  busy_timeout: 5000
+  busy_timeout: 10_000
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
